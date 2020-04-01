471bd5cd120370fe5bcf690c8cfca3dff6c79247
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-04-01 21:41:18 +0200

    Le liste ora sono a comparsa

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-// $(".ok").click(
-//   function(){
-//     $(".casper").toggle();
-//   }
-// )
-
+// rendo le liste invisibili
 $(".casper").hide();
+// creo una funzione che rende le liste
+// a comparsa tramite click
+$(".ok h3").click(
+  function(){
+    $(this).siblings('.casper').toggle();
+  }
+);
